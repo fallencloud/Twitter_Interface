8b0342d0d61c787a43de36d000b6dae0fba1b8a9
Sharina V. Jones <sharinavjones@gmail.com>
2018-07-18 13:38:06 -0400

    Verified authentication

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 const express = require('express');
+const request = require('request');
+const Twit = require('twit');
+const $ = require('cheerio');
+
+const app = express();
+const {conKey} = require('./config.js');
+const {conSecret} = require('./config.js');
+const {accToken} = require('./config.js');
+const {accSecret} = require('./config.js');
+const port = 3000;
+
+//declarations
+const {myId} = require('./config.js');
+
+const T = new Twit({
+  consumer_key:         conKey,
+  consumer_secret:      conSecret,
+  access_token:         accToken,
+  access_token_secret:  accSecret
+});
+
+const options = {screen_name: myId, count : 5};
+function getTweets(err, data, response){
+  console.log(data);
+}
+
+//T.get(`statuses/lookup/:${myId}`, options, getTweets);
+
+
+T.get(`GET statuses/user_timeline`, {screen_name: myId, count: 5}, function(err, data, response) {
+  console.log(data)
+})
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+})
